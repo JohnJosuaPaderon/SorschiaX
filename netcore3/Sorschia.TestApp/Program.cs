c5c2970d982b5d1b909e24cb6d4f3cf97e51cd36
John Josua Paderon <johnjosua.paderon@outlook.com>
2020-07-06 00:14:00 +0800

    Test for RSA Assymetric Crypto

--- netcore3/Sorschia.TestApp/Program.cs
+++ netcore3/Sorschia.TestApp/Program.cs
@@ -8,15 +8,40 @@ namespace Sorschia.TestApp
     {
         static void Main(string[] args)
         {
-            var aesCrypto = new AesCrypto();
-            var plainText = "johnjosua.paderon";
-            var cryptoKeyString = "a";
+            //Sorschia.Security.AesCrypto
 
-            var cipherString = aesCrypto.Encrypt(plainText, cryptoKeyString);
-            Console.WriteLine("Cipher: {0}", cipherString.GetCipherText());
-            Console.WriteLine("IV: {0}", cipherString.GetIvText());
-            Console.WriteLine("Salt: {0}", cipherString.GetSaltText());
-            Console.WriteLine(aesCrypto.Decrypt(cipherString.GetCipherText(), cryptoKeyString));
+            //var aesCrypto = new AesCrypto();
+            //var plainText = "johnjosua.paderon";
+            //var cryptoKeyString = "a";
+
+            //var cipherString = aesCrypto.Encrypt(plainText, cryptoKeyString);
+            //Console.WriteLine("Cipher: {0}", cipherString.GetCipherText());
+            //Console.WriteLine("IV: {0}", cipherString.GetIvText());
+            //Console.WriteLine("Salt: {0}", cipherString.GetSaltText());
+            //Console.WriteLine(aesCrypto.Decrypt(cipherString.GetCipherText(), cryptoKeyString));
+
+            //Sorschia.Security.RsaCrypto
+
+            var rsaCrypto = new RsaCrypto();
+            var publicKeyStore = new RsaPublicKeyStore();
+            var privateKeyStore = new RsaPrivateKeyStore(publicKeyStore);
+            var rsaKeys1 = privateKeyStore.Initialize("user-username");
+            var rsaKeys2 = privateKeyStore.Initialize("user-password");
+
+            Console.WriteLine(rsaKeys1.PublicKey);
+            Console.WriteLine(rsaKeys2.PublicKey);
+
+            Console.WriteLine(rsaKeys1.PrivateKey);
+            Console.WriteLine(rsaKeys2.PrivateKey);
+
+            var cipherData = rsaCrypto.Encrypt(Encoding.UTF8.GetBytes("johnjosua.paderon"), privateKeyStore.Request("user-username"));
+            var cipherText = Convert.ToBase64String(cipherData);
+            Console.WriteLine(cipherText);
+
+            var data = rsaCrypto.Decrypt(Convert.FromBase64String(cipherText), privateKeyStore.Request("user-username"));
+            var text = Encoding.UTF8.GetString(data);
+            Console.WriteLine(text);
+
             Console.ReadKey();
         }
     }
