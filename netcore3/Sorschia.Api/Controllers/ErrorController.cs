24d5301d3b4a1d6628b893b8c23c89d2b9e2d903
John Josua Paderon <johnjosua.paderon@outlook.com>
2020-07-19 23:20:55 +0800

    Added error controller

---
+++ netcore3/Sorschia.Api/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sorschia.Controllers
+{
+    [ApiController]
+    public sealed class ErrorController : ControllerBase
+    {
+        [Route("/error")]
+        public IActionResult Error()
+        {
+            var feature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+
+            if (feature.Error is SorschiaException sorschiaException && sorschiaException.IsMessageViewable)
+            {
+                return Problem(feature.Error.Message, type: ApiConstants.CustomErrorContentType);
+            }
+            else
+            {
+                return Problem();
+            }
+        }
+    }
+}
