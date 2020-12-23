fe0955bfddff1eb9fc141e2807cfbe02ce66490d
John Josua Paderon <johnjosua.paderon@outlook.com>
2020-12-23 16:12:24 +0800

    DELETED ALL FILES

--- net5/Sorschia/Utilities/IDataTranslator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sorschia.Utilities
-{
-    public interface IDataTranslator<TData1, TData2>
-    {
-        TData1 Translate(TData2 source);
-        TData2 Translate(TData1 source);
-    }
-}
