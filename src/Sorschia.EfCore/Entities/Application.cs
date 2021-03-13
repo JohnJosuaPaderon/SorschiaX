9eb559ea2a1cfdab933ae055cc5d96fd2358e5f0
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-03-13 11:24:31 +0800

    EF Core Entiities

---
+++ src/Sorschia.EfCore/Entities/Application.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Sorschia.Entities
+{
+    public class Application : EntityBase
+    {
+        public short Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Role> Roles { get; set; } = new List<Role>();
+        public ICollection<Permission> Permissions { get; set; } = new List<Permission>();
+    }
+}
