2a05b20550b24999fb0deb27bb6dd7df91ad6fc9
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-03-13 11:25:16 +0800

    Sorschia Ef Core Project type

---
+++ src/Sorschia.EfCore/SorschiaEfCoreOptions.cs
@@ -0,0 +1,13 @@
+﻿using Sorschia.Data;
+
+namespace Sorschia
+{
+    public sealed class SorschiaEfCoreOptions
+    {
+        public DataOptions Data { get; set; } = new DataOptions();
+
+        internal SorschiaEfCoreOptions()
+        {
+        }
+    }
+}
