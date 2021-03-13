e5b2bb5dcd870b587e7622a6f23468465e6017d7
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-03-13 11:25:42 +0800

    EF Core library dependency injector

---
+++ src/Sorschia.EfCore.DependencyInjector/IServiceCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sorschia.Data;
+using System;
+
+namespace Sorschia
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSorschiaEfCore(this IServiceCollection instance, Action<SorschiaEfCoreOptions> configure = null)
+        {
+            var options = new SorschiaEfCoreOptions();
+            configure?.Invoke(options);
+
+            instance.AddSingleton(options.Data ?? new DataOptions());
+
+            return instance;
+        }
+    }
+}
