ffebb64433bf0f41d86dab98b551f90e8ebb8024
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-03-21 23:42:01 +0800

    Revert "Revert "Nullable MiddleName & NameSuffix""
    This reverts commit efbdfa25b7f57ed9307d85f5cf2a8d2aad33cff4.

--- src/Sorschia/Utilities/IFullName.cs
+++ src/Sorschia/Utilities/IFullName.cs
@@ -3,9 +3,9 @@
     public interface IFullName
     {
         string FirstName { get; set; }
-        string MiddleName { get; set; }
+        string? MiddleName { get; set; }
         string LastName { get; set; }
-        string NameSuffix { get; set; }
+        string? NameSuffix { get; set; }
         string FullName { get; set; }
     }
 }
