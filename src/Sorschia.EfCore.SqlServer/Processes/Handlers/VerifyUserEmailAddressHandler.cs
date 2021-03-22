eed7981418a7d58564166e8986f9972ab849414d
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-03-22 16:03:21 +0800

    Verify user email address

---
+++ src/Sorschia.EfCore.SqlServer/Processes/Handlers/VerifyUserEmailAddressHandler.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Sorschia.Data;
+using Sorschia.Entities;
+using Sorschia.Extensions;
+using Sorschia.Utilities;
+using Sorschia.Utilities.ExceptionBuilders;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sorschia.Processes.Handlers
+{
+    internal sealed class VerifyUserEmailAddressHandler : IRequestHandler<VerifyUserEmailAddress>
+    {
+        private readonly IDbContextFactory<SorschiaDbContext> _contextFactory;
+        private readonly ICurrentFootprintProvider _currentFootprintProvider;
+
+        public VerifyUserEmailAddressHandler(IDbContextFactory<SorschiaDbContext> contextFactory, ICurrentFootprintProvider currentFootprintProvider)
+        {
+            _contextFactory = contextFactory;
+            _currentFootprintProvider = currentFootprintProvider;
+        }
+
+        public async Task<Unit> Handle(VerifyUserEmailAddress request, CancellationToken cancellationToken)
+        {
+            using var context = _contextFactory.CreateDbContext();
+            using var transaction = await context.Database.BeginTransactionAsync(cancellationToken);
+            var footprint = _currentFootprintProvider.Current;
+            var user = await context.GetUserAsync(request.UserId, cancellationToken: cancellationToken);
+
+            if (user.IsEmailAddressVerified == request.IsVerified)
+                throw new SorschiaEntityFieldAlreadySetExceptionBuilder()
+                    .WithEntityType<User>()
+                    .AddEntityField("IsEmailAddressVerified", request.IsVerified)
+                    .Build();
+
+            user.IsEmailAddressVerified = request.IsVerified;
+            context.Update(user)
+                .SetUpdateFootprint(footprint);
+            await context.SaveChangesAsync(cancellationToken);
+            await transaction.CommitAsync(cancellationToken);
+            return Unit.Value;
+        }
+    }
+}
---
+++ src/Sorschia.FluentValidation/Processes/Validators/VerifyUserEmailAddressValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Sorschia.Processes.Validators
+{
+    public sealed class VerifyUserEmailAddressValidator : AbstractValidator<VerifyUserEmailAddress>
+    {
+        public VerifyUserEmailAddressValidator()
+        {
+            RuleFor(_ => _.UserId)
+                .NotEqual(0);
+        }
+    }
+}
