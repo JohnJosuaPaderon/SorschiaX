a4dda154ed17074633284091442f98fb28654830
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-03-22 16:03:31 +0800

    Verify user mobile number

---
+++ src/Sorschia.FluentValidation/Processes/Validators/VerifyUserMobileNumberValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Sorschia.Processes.Validators
+{
+    public sealed class VerifyUserMobileNumberValidator : AbstractValidator<VerifyUserMobileNumber>
+    {
+        public VerifyUserMobileNumberValidator()
+        {
+            RuleFor(_ => _.UserId)
+                .NotEqual(0);
+        }
+    }
+}
---
+++ src/Sorschia/Processes/VerifyUserMobileNumber.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Sorschia.Processes
+{
+    public class VerifyUserMobileNumber : IRequest
+    {
+        public int UserId { get; set; }
+        public bool IsVerified { get; set; }
+    }
+}
