dd07d1fbd62730b17acc6cf6714a7f8f7b4881dc
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-03-22 16:17:11 +0800

    Moved exception builders to Sorschia.Entities.ExceptionBuilders

--- src/Sorschia/Entities/ExceptionBuilders/SorschiaEntityNotFoundExceptionBuilder.cs
+++ src/Sorschia/Entities/ExceptionBuilders/SorschiaEntityNotFoundExceptionBuilder.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Sorschia.Utilities.ExceptionBuilders
+namespace Sorschia.Entities.ExceptionBuilders
 {
     public sealed class SorschiaEntityNotFoundExceptionBuilder
     {
