ac8aeb98e380d3793dce6db784aa5c0594fbace1
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-03-22 15:25:44 +0800

    Exception to throw if setting entity's field if the current value and the new value are same

---
+++ src/Sorschia/SorschiaEntityFieldAlreadySetException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Sorschia
+{
+    public class SorschiaEntityFieldAlreadySetException : SorschiaException
+    {
+        public Type? EntityType { get; }
+        public IDictionary<string, object> EntityFields { get; }
+
+        public SorschiaEntityFieldAlreadySetException(Type? entityType, IDictionary<string, object> entityFields, bool isUserFriendlyMessage = false) : base(isUserFriendlyMessage)
+        {
+            EntityType = entityType;
+            EntityFields = entityFields;
+        }
+
+        public SorschiaEntityFieldAlreadySetException(Type? entityType, IDictionary<string, object> entityFields, string message, bool isUserFriendlyMessage = false) : base(message, isUserFriendlyMessage)
+        {
+            EntityType = entityType;
+            EntityFields = entityFields;
+        }
+
+        public SorschiaEntityFieldAlreadySetException(Type? entityType, IDictionary<string, object> entityFields, string message, Exception innerException, bool isUserFriendlyMessage = false) : base(message, innerException, isUserFriendlyMessage)
+        {
+            EntityType = entityType;
+            EntityFields = entityFields;
+        }
+
+        protected SorschiaEntityFieldAlreadySetException(Type? entityType, IDictionary<string, object> entityFields, SerializationInfo info, StreamingContext context, bool isUserFriendlyMessage = false) : base(info, context, isUserFriendlyMessage)
+        {
+            EntityType = entityType;
+            EntityFields = entityFields;
+        }
+    }
+}
---
+++ src/Sorschia/Utilities/ExceptionBuilders/SorschiaEntityFieldAlreadySetExceptionBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sorschia.Utilities.ExceptionBuilders
+{
+    public sealed class SorschiaEntityFieldAlreadySetExceptionBuilder
+    {
+        public Type? EntityType { get; private set; }
+        public IDictionary<string, object> EntityFields { get; } = new Dictionary<string, object>();
+
+        public SorschiaEntityFieldAlreadySetExceptionBuilder WithEntityType(Type entityType)
+        {
+            EntityType = entityType;
+            return this;
+        }
+
+        public SorschiaEntityFieldAlreadySetExceptionBuilder WithEntityType<T>()
+        {
+            WithEntityType(typeof(T));
+            return this;
+        }
+
+        public SorschiaEntityFieldAlreadySetExceptionBuilder AddEntityField(string key, object value)
+        {
+            EntityFields.Add(key, value);
+            return this;
+        }
+
+        public SorschiaEntityFieldAlreadySetException Build()
+        {
+            return new SorschiaEntityFieldAlreadySetException(EntityType, EntityFields);
+        }
+    }
+}
