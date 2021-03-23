efa6e6682ffa496b0667195662582918136daa01
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-03-23 13:42:44 +0800

    Dapper

---
+++ src/Sorschia.Dapper.SqlServer/Data/SqlConnectionProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sorschia.Data
+{
+    internal sealed class SqlConnectionProvider
+    {
+        private readonly IConfiguration _configuration;
+
+        public SqlConnectionProvider(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<SqlConnection> OpenAsync(string configKey, CancellationToken  cancellationToken = default)
+        {
+            var connection = new SqlConnection(_configuration.GetConnectionString(configKey));
+            await connection.OpenAsync(cancellationToken);
+            return connection;
+        }
+
+        public async Task<SqlConnection> OpenAsync(CancellationToken cancellationToken = default)
+        {
+            return await OpenAsync("Default", cancellationToken);
+        }
+    }
+}
---
+++ src/Sorschia.Dapper.SqlServer/Processes/Handlers/GetUserHandler.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using MediatR;
+using Sorschia.Data;
+using Sorschia.Entities;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sorschia.Processes.Handlers
+{
+    internal sealed class GetUserHandler : IRequestHandler<GetUser, GetUser.Result>
+    {
+        private readonly SqlConnectionProvider _connectionProvider;
+
+        public GetUserHandler(SqlConnectionProvider connectionProvider)
+        {
+            _connectionProvider = connectionProvider;
+        }
+
+        public async Task<GetUser.Result> Handle(GetUser request, CancellationToken cancellationToken)
+        {
+            using var connection = await _connectionProvider.OpenAsync(cancellationToken);
+            using var reader = await connection.QueryMultipleAsync("dbo.GetUser");
+        }
+    }
+}
