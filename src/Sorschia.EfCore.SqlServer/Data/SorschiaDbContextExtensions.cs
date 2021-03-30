fafdd8c34cf3c64a420b2b6da28daa8ff9f635bc
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-03-30 17:36:37 +0800

    Eliminated magic string

--- src/Sorschia.EfCore.SqlServer/Data/SorschiaDbContextExtensions.cs
+++ src/Sorschia.EfCore.SqlServer/Data/SorschiaDbContextExtensions.cs
@@ -7,14 +7,16 @@ namespace Sorschia.Data
 {
     internal static class SorschiaDbContextExtensions
     {
-        public static async Task<User> GetUserAsync(this SorschiaDbContext instance, int id, bool throwEntityNotFoundException = true, CancellationToken cancellationToken = default)
+        public static async Task<User> GetUserAsync(this SorschiaDbContext instance, int id, bool throwEntityNotFoundException = true, CancellationToken? cancellationToken = default)
         {
+            if (id == 0) return null;
+
             var user = await instance.Users.FindAsync(new object[] { id }, cancellationToken);
 
             if (throwEntityNotFoundException && user is null)
                 throw new EntityNotFoundExceptionBuilder()
                     .WithEntityType<User>()
-                    .AddLookupField("Id", id)
+                    .AddField(nameof(User.Id), id)
                     .Build();
 
             return user;
