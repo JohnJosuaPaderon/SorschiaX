f6ea0759102f77f994ceabf35784a8bf07b3cc26
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-04-06 20:54:31 +0800

    Full Name

---
+++ src/Sorschia/Entities/IFullNameBuilder.cs
@@ -0,0 +1,8 @@
+﻿namespace Sorschia.Entities
+{
+    public interface IFullNameBuilder
+    {
+        string Build(string firstName, string middleName, string lastName, string nameSuffix);
+        string Build(IFullNameHolder holder);
+    }
+}
