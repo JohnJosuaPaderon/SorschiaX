5160e78ee98743e8279c20dd5da09298bf3f92da
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-05-16 04:45:36 +0800

    Exceptions & Builders

---
+++ src/Sorschia/Exceptions/Builders/EntityNotFoundExceptionBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Sorschia.Exceptions.Builders
+{
+    public sealed class EntityNotFoundExceptionBuilder : EntityExceptionBuilderBase<EntityNotFoundExceptionBuilder>, IExceptionBuilder<EntityNotFoundException>
+    {
+        public List<EntityField> Fields { get; } = new();
+        protected override EntityNotFoundExceptionBuilder Self => this;
+
+        public EntityNotFoundException Build() => new(Message, InnerException, DebugMessage, EntityType, Fields);
+
+        public EntityNotFoundExceptionBuilder AddField(EntityField field)
+        {
+            Fields.Add(field);
+            return Self;
+        }
+
+        public EntityNotFoundExceptionBuilder AddField(string name, object? value) => AddField(new EntityField(name, value));
+
+        public EntityNotFoundExceptionBuilder AddField(string name) => AddField(name, null);
+    }
+}
---
+++ src/Sorschia/Exceptions/EntityException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Sorschia.Exceptions
+{
+    public class EntityException : SorschiaException
+    {
+        public Type? EntityType { get; }
+
+        public EntityException(string? message, Exception? innerException, string? debugMessage, Type? entityType) : base(message, innerException, debugMessage)
+        {
+            EntityType = entityType;
+        }
+    }
+}
