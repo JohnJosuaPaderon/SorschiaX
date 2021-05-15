d22e3005392e93557d42d296fcec1cdd9d232c63
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-05-16 03:17:36 +0800

    Sorschia.Core

---
+++ src/Sorschia.Core/Entities/Domain.cs
@@ -0,0 +1,12 @@
+﻿namespace Sorschia.Core.Entities
+{
+    public class Domain : DomainBase
+    {
+    }
+
+    public class DomainBase
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+    }
+}
