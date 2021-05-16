acf0d3cf3bd27f006a540dd10dcb2351ab21808c
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-05-16 15:06:33 +0800

    Sorschia.EntityFrameworkCore

---
+++ src/Sorschia.EntityFrameworkCore/Extensions/DbSetExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Sorschia.Audit;
+
+namespace Sorschia.Extensions
+{
+    public static class DbSetExtensions
+    {
+        public static EntityEntry<TEntity> AddWithFootprint<TEntity>(this DbSet<TEntity> instance, TEntity entity, IFootprint footprint) where TEntity : class
+        {
+            return instance.Add(entity)
+                .SetInsertFootprint(footprint);
+        }
+
+        public static EntityEntry<TEntity> AddWithFootprint<TEntity>(this DbSet<TEntity> instance, TEntity entity) where TEntity : class
+        {
+            return instance.Add(entity)
+                .SetInsertFootprint();
+        }
+    }
+}
