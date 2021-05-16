b8ae151dad8e8be6019eddb49d8e76e07ca042df
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-05-16 11:53:13 +0800

    Insert Domain Handler

---
+++ src/core/Sorschia.Core.EntityFrameworkCore.SqlServer/Processes/Handlers/DbInsertDomainHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Sorschia.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sorschia.Core.Processes.Handlers
+{
+    internal sealed class DbInsertDomainHandler : IRequestHandler<DbInsertDomain.Request, Domain>
+    {
+        public Task<Domain> Handle(DbInsertDomain.Request request, CancellationToken cancellationToken)
+        {
+            var context = request.DbContext;
+
+
+        }
+    }
+}
