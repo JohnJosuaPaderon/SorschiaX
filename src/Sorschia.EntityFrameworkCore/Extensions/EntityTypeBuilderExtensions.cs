7a6ad864df6165703e8f887c31a704940aa1271e
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-05-31 01:12:28 +0800

    Soft-Delete and Footprint shadow property configuration

---
+++ src/Sorschia.EntityFrameworkCore/Extensions/EntityTypeBuilderExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sorschia.Data;
+using System;
+
+namespace Sorschia.Extensions
+{
+    public static class EntityTypeBuilderExtensions
+    {
+        public static EntityTypeBuilder<TEntity> HasSoftDelete<TEntity>(this EntityTypeBuilder<TEntity> instance, bool hasFootprint = true) where TEntity : class
+        {
+            instance.Property<bool>(ShadowProperties.IsDeleted);
+
+            if (hasFootprint)
+                instance.HasDeleteFootprint();
+
+            return instance;
+        }
+
+        public static EntityTypeBuilder<TEntity> HasInsertFootprint<TEntity>(this EntityTypeBuilder<TEntity> instance) where TEntity : class
+        {
+            instance.Property<int?>(ShadowProperties.InsertedById);
+            instance.Property<DateTimeOffset?>(ShadowProperties.InsertedOn);
+            return instance;
+        }
+
+        public static EntityTypeBuilder<TEntity> HasUpdateFootprint<TEntity>(this EntityTypeBuilder<TEntity> instance) where TEntity : class
+        {
+            instance.Property<int?>(ShadowProperties.UpdatedById);
+            instance.Property<DateTimeOffset?>(ShadowProperties.UpdatedOn);
+            return instance;
+        }
+
+        public static EntityTypeBuilder<TEntity> HasDeleteFootprint<TEntity>(this EntityTypeBuilder<TEntity> instance) where TEntity : class
+        {
+            instance.Property<int?>(ShadowProperties.DeletedById);
+            instance.Property<DateTimeOffset?>(ShadowProperties.DeletedOn);
+            return instance;
+        }
+    }
+}
