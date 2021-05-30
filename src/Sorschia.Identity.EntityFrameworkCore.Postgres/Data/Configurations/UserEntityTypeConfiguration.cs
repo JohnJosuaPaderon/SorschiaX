ad5aea710ddffc16b6d168703ee661907e8e4c86
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-05-31 01:40:08 +0800

    Postgres

---
+++ src/Sorschia.Identity.EntityFrameworkCore.Postgres/Data/Configurations/UserEntityTypeConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sorschia.Identity.Entities;
+
+namespace Sorschia.Identity.Data.Configurations
+{
+    internal sealed class UserEntityTypeConfiguration : UserEntityTypeConfigurationBase, IEntityTypeConfiguration<User>
+    {
+        public override void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("User", "Identity");
+            base.Configure(builder);
+        }
+    }
+}
---
+++ src/Sorschia.Identity.EntityFrameworkCore/Data/Configurations/UserEntityTypeConfigurationBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sorschia.Extensions;
+using Sorschia.Identity.Entities;
+
+namespace Sorschia.Identity.Data.Configurations
+{
+    public abstract class UserEntityTypeConfigurationBase
+    {
+        public virtual void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder
+                .HasSoftDelete()
+                .HasInsertFootprint()
+                .HasUpdateFootprint()
+                .HasDeleteFootprint();
+        }
+    }
+}
