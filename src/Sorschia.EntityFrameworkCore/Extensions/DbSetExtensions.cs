33dfcf3dcd1722e32aeeb68d59603307287b5cc9
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-05-31 00:40:52 +0800

    Footprints

--- src/Sorschia.EntityFrameworkCore/Extensions/DbSetExtensions.cs
+++ src/Sorschia.EntityFrameworkCore/Extensions/DbSetExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Sorschia.Auditing;
 using Sorschia.ErrorHandling.Builders;
 using System.Threading;
 using System.Threading.Tasks;
@@ -33,6 +35,30 @@ namespace Sorschia.Extensions
         public static async Task<TEntity> FindByIdAsync<TEntity, TId>(this DbSet<TEntity> instance, TId id, CancellationToken cancellationToken = default) where TEntity : class
         {
             return await instance.FindByIdAsync(id, null, null, cancellationToken);
+        }
+
+        public static EntityEntry<TEntity> SoftDelete<TEntity>(this DbSet<TEntity> instance, TEntity entity) where TEntity : class
+        {
+            return instance.Update(entity)
+                .SetDeleted();
+        }
+
+        public static EntityEntry<TEntity> SoftDelete<TEntity>(this DbSet<TEntity> instance, TEntity entity, Footprint footprint) where TEntity : class
+        {
+            return instance.SoftDelete(entity)
+                .SetDeleteFootprint(footprint);
+        }
+
+        public static EntityEntry<TEntity> Add<TEntity>(this DbSet<TEntity> instance, TEntity entity, Footprint footprint) where TEntity : class
+        {
+            return instance.Add(entity)
+                .SetInsertFootprint(footprint);
+        }
+
+        public static EntityEntry<TEntity> Update<TEntity>(this DbSet<TEntity> instance, TEntity entity, Footprint footprint) where TEntity : class
+        {
+            return instance.Update(entity)
+                .SetUpdateFootprint(footprint);
         }
     }
 }
