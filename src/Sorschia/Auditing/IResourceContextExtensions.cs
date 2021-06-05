4d5a4be9ac7c09df82e5b664ad2be2a3b0347df0
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-06-05 08:47:06 +0800

    Get & set Current Footprint

---
+++ src/Sorschia/Auditing/IResourceContextExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Sorschia.Utilities;
+
+namespace Sorschia.Auditing
+{
+    public static class IResourceContextExtensions
+    {
+        private const string CurrentFootprint = "Sorschia.Auditing.Footprint|Current";
+        
+        public static IResourceContext SetCurrentFootprint(this IResourceContext instance, Footprint footprint) => instance.Register(CurrentFootprint, footprint);
+
+        public static Footprint GetCurrentFootprint(this IResourceContext instance) => instance.Request<Footprint>(CurrentFootprint);
+    }
+}
