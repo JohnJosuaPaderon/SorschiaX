cfa15a79c6a3edf8b163651a751df16e2a20ffef
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-06-05 08:50:55 +0800

    AsInsertPermissionRequest()

---
+++ src/Sorschia.Identity.EntityFrameworkCore/Processing/Requests/AddPermissionRequestExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace Sorschia.Identity.Processing.Requests
+{
+    internal static class AddPermissionRequestExtensions
+    {
+        public static InsertPermissionRequest AsInsertPermissionRequest(this AddPermissionRequest instance) => new()
+        {
+            Name = instance.Name,
+            LookupCode = instance.LookupCode,
+            Description = instance.Description
+        };
+    }
+}
