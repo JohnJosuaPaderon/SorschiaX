fbb099a9996da1d9a82adf5064b0101c47949e95
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-06-05 08:48:11 +0800

    Add Permission Request & Response

---
+++ src/Sorschia.Identity/Processing/Requests/AddPermissionRequest.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Sorschia.Identity.Processing.Responses;
+
+namespace Sorschia.Identity.Processing.Requests
+{
+    public class AddPermissionRequest : IRequest<AddPermissionResponse>
+    {
+        public string Name { get; set; }
+        public string LookupCode { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/Sorschia.Identity/Processing/Responses/AddPermissionResponse.cs
@@ -0,0 +1,20 @@
+﻿using Sorschia.Identity.Entities;
+
+namespace Sorschia.Identity.Processing.Responses
+{
+    public class AddPermissionResponse : PermissionBase
+    {
+        public AddPermissionResponse From(Permission permission)
+        {
+            if (permission is not null)
+            {
+                Id = permission.Id;
+                Name = permission.Name;
+                LookupCode = permission.LookupCode;
+                Description = permission.Description;
+            }
+
+            return this;
+        }
+    }
+}
