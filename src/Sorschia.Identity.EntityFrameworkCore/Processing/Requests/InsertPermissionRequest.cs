a0df0f437d8b2dda9d6ac93940294c5be85d3902
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-06-05 08:49:44 +0800

    Insert Permission Request

---
+++ src/Sorschia.Identity.EntityFrameworkCore/Processing/Requests/InsertPermissionRequest.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Sorschia.Identity.Entities;
+using Sorschia.Utilities;
+
+namespace Sorschia.Identity.Processing.Requests
+{
+    internal class InsertPermissionRequest : IRequest<Permission>, IResourceConsumer
+    {
+        public ResourceConsumerIdentifier Id { get; }
+        public string Name { get; set; }
+        public string LookupCode { get; set; }
+        public string Description { get; set; }
+
+        public Permission AsPermission() => new()
+        {
+            Name = Name,
+            LookupCode = LookupCode,
+            Description = Description
+        };
+    }
+}
