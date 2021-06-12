137394661682c39d9625fbfb6b28617270e05dbd
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-06-13 01:13:36 +0800

    Insert User-Permission

---
+++ src/Sorschia.Identity.EntityFrameworkCore/Processing/Handlers/InsertUserPermissionHandler.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Sorschia.Auditing;
+using Sorschia.ErrorHandling.Builders;
+using Sorschia.Extensions;
+using Sorschia.Identity.Data;
+using Sorschia.Identity.Entities;
+using Sorschia.Identity.Processing.Requests;
+using Sorschia.Utilities;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sorschia.Identity.Processing.Handlers
+{
+    internal sealed class InsertUserPermissionHandler : IRequestHandler<InsertUserPermissionRequest, UserPermission>
+    {
+        private readonly IResourceManager _resourceManager;
+
+        public InsertUserPermissionHandler(IResourceManager resourceManager)
+        {
+            _resourceManager = resourceManager;
+        }
+
+        public async Task<UserPermission> Handle(InsertUserPermissionRequest request, CancellationToken cancellationToken)
+        {
+            var resourceContext = _resourceManager.GetContext(request);
+            var context = resourceContext.GetIdentityContext();
+            var currentFootprint = resourceContext.GetCurrentFootprint();
+            var user = request.User ?? await context.FindUserAsync(request.UserId, cancellationToken);
+            var permission = request.Permission ?? await context.FindPermissionAsync(request.PermissionId, cancellationToken);
+
+            if (await context.UserPermissions.Where(_ => _.UserId == request.UserId && _.PermissionId == request.PermissionId).AnyAsync(cancellationToken))
+                throw new DuplicateEntityExceptionBuilder()
+                    .WithEntityType<UserPermission>()
+                    .WithMessage("User-Permission already exists")
+                    .WithDebugMessage($"User-Permission with UserId = {request.UserId}, PermissionId = {request.PermissionId} already exists")
+                    .AddField(nameof(UserPermission.UserId), request.UserId)
+                    .AddField(nameof(UserPermission.PermissionId), request.PermissionId)
+                    .Build();
+
+            var userPermission = request.AsUserPermission();
+            userPermission.User = user;
+            userPermission.Permission = permission;
+            context.UserPermissions.Add(userPermission, currentFootprint);
+            await context.SaveChangesAsync(cancellationToken);
+            return userPermission;
+        }
+    }
+}
---
+++ src/Sorschia.Identity.EntityFrameworkCore/Processing/Requests/InsertUserPermissionRequest.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Sorschia.Identity.Entities;
+using Sorschia.Utilities;
+
+namespace Sorschia.Identity.Processing.Requests
+{
+    internal class InsertUserPermissionRequest : IRequest<UserPermission>, IResourceConsumer
+    {
+        ResourceConsumerIdentifier IResourceConsumer.Id { get; }
+        public User User { get; set; }
+        public int UserId { get; set; }
+        public Permission Permission { get; set; }
+        public int PermissionId { get; set; }
+
+        public UserPermission AsUserPermission() => new()
+        {
+            UserId = UserId,
+            PermissionId = PermissionId
+        };
+    }
+}
